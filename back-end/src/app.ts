69d1c763d112cc0e67fe6a2c2d3dab7e98dafe79
Enio Schaefer <enio100schaefer@hotmail.com>
2023-11-30 03:24:37 -0300

    config: setup basico de back-end com Node, Express e Typescript

---
+++ back-end/src/app.ts
@@ -0,0 +1,17 @@
+// index.ts
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+
+const app = express();
+const port = 3001;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
